e56476d83f97806b84b2c2dab8c948191f788ac9
Daniel <danieladigamov72@gmail.com>
2024-10-17 16:21:07 +0100

    Started cw

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,7 @@
 package gol
 
+import "uk.ac.bris.cs/gameoflife/util"
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -13,14 +15,20 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
 
 	turn := 0
 	c.events <- StateChange{turn, Executing}
 
 	// TODO: Execute all turns of the Game of Life.
-
+	for i := 0; i < p.Turns; i++ {
+		world = calculateNextState(p, world)
+	}
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
+	
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
@@ -29,4 +37,61 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+
+	nextWorld := make([][]byte, p.ImageHeight)
+
+	for i := range nextWorld {
+		nextWorld[i] = make([]byte, p.ImageWidth)
+	}
+
+	countAlive := func(y, x int) int {
+		alive := 0
+		for i := -1; i < 2; i++ {
+			for j := -1; j < 2; j++ {
+				neighbourY := (y + i + p.ImageHeight) % p.ImageHeight
+				neighbourX := (x + j + p.ImageWidth) % p.ImageWidth
+				if !(i == 0 && j == 0) && (world[neighbourY][neighbourX] == 255) {
+					alive++
+				}
+			}
+		}
+		return alive
+	}
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			aliveNeighbour := countAlive(y, x)
+
+			if world[y][x] == 255 {
+				if aliveNeighbour < 2 || aliveNeighbour > 3 {
+					nextWorld[y][x] = 0
+				} else {
+					nextWorld[y][x] = 255
+				}
+			} else {
+				if aliveNeighbour == 3 {
+					nextWorld[y][x] = 255
+				} else {
+					nextWorld[y][x] = 0
+				}
+			}
+		}
+	}
+
+	return nextWorld
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	var alive []util.Cell
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 255 {
+				alive = append(alive, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return alive
 }
