9ecc0376ad124a2939edff690950b90ec81cc33f
MykolaTakun <takun1812@gmail.com>
2024-10-19 14:39:04 +0100

    Revert "Merge branch 'dev' into mykola-test"
    This reverts commit 39c445270fa862d8aa61e249fb1a30ccde91f5ed, reversing
    changes made to 32882783edf9307e6dcbe48dea4eefcb071da96f.

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,10 +1,5 @@
 package gol
 
-import (
-	"fmt"
-	"uk.ac.bris.cs/gameoflife/util"
-)
-
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -18,34 +13,13 @@ type distributorChannels struct {
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
-	world := make([][]byte, p.ImageHeight)
-	for i := range world {
-		world[i] = make([]byte, p.ImageWidth)
-	}
-
-	c.ioCommand <- ioInput
-	c.ioFilename <- fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight)
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			world[y][x] = <-c.ioInput
-		}
-	}
 
 	turn := 0
 	c.events <- StateChange{turn, Executing}
 
 	// TODO: Execute all turns of the Game of Life.
-	for turn := 0; turn < p.Turns; turn++ {
-		world = calculateNextState(p, world)
-	}
+
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
-	finalAliveCells := make([]util.Cell, p.ImageWidth*p.ImageHeight)
-	_, finalAliveCells = calculateAliveCells(p, world)
-
-	finalstate := FinalTurnComplete{p.Turns, finalAliveCells}
-
-	c.events <- finalstate
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
@@ -55,91 +29,4 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
-}
-
-func calculateNextState(p Params, world [][]byte) [][]byte {
-
-	nextWorld := make([][]byte, p.ImageHeight)
-
-	for i := range nextWorld {
-		nextWorld[i] = make([]byte, p.ImageWidth)
-	}
-
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			// Sum up the alive neighbors
-			sum := int(world[(y+p.ImageHeight-1)%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 +
-				int(world[(y+p.ImageHeight-1)%p.ImageHeight][x%p.ImageWidth])/255 +
-				int(world[(y+p.ImageHeight-1)%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255 +
-				int(world[y%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 +
-				int(world[y%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255 +
-				int(world[(y+p.ImageHeight+1)%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 +
-				int(world[(y+p.ImageHeight+1)%p.ImageHeight][x%p.ImageWidth])/255 +
-				int(world[(y+p.ImageHeight+1)%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255
-
-			if world[y][x] == 255 {
-				if sum < 2 || sum > 3 {
-					nextWorld[y][x] = 0 // Cell dies
-				} else {
-					nextWorld[y][x] = 255 // Cell stays alive
-				}
-			} else {
-				if sum == 3 {
-					nextWorld[y][x] = 255 // Dead cell becomes alive
-				} else {
-					nextWorld[y][x] = 0 // Dead cell stays dead
-				}
-			}
-		}
-	}
-
-	countAlive := func(y, x int) int {
-		alive := 0
-		for i := -1; i < 2; i++ {
-			for j := -1; j < 2; j++ {
-				neighbourY := (y + i + p.ImageHeight) % p.ImageHeight
-				neighbourX := (x + j + p.ImageWidth) % p.ImageWidth
-				if !(i == 0 && j == 0) && (world[neighbourY][neighbourX] == 255) {
-					alive++
-				}
-			}
-		}
-		return alive
-	}
-
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			aliveNeighbour := countAlive(y, x)
-
-			if world[y][x] == 255 {
-				if aliveNeighbour < 2 || aliveNeighbour > 3 {
-					nextWorld[y][x] = 0
-				} else {
-					nextWorld[y][x] = 255
-				}
-			} else {
-				if aliveNeighbour == 3 {
-					nextWorld[y][x] = 255
-				} else {
-					nextWorld[y][x] = 0
-				}
-			}
-		}
-	}
-
-	return nextWorld
-}
-
-func calculateAliveCells(p Params, world [][]byte) (int, []util.Cell) {
-	var alive []util.Cell
-	count := 0
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			if world[y][x] == 255 {
-				count++
-				alive = append(alive, util.Cell{X: x, Y: y})
-			}
-		}
-	}
-	return count, alive
 }
